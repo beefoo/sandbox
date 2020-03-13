abb9dfed1f5536ad390a49b5df9e9e3c23a69360
beefoo <brian@youaremyjoy.org>
2020-03-13 11:42:34 -0400

    add digitized col to schwartz

--- schwartz/process_data.py
+++ schwartz/process_data.py
@@ -12,12 +12,37 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('-in', dest="INPUT_FILE", default="cabinet-7037712/titles/*", help="Input files")
 parser.add_argument('-out', dest="OUTPUT_FILE", default="output/cabinet-7037712.csv", help="Output file")
+parser.add_argument('-ann', dest="ANN_FILE", default="output/cabinet-7037712_annotated.csv", help="Annotation file")
 parser.add_argument('-probe', dest="PROBE", action="store_true", help="Just output command strings?")
 a = parser.parse_args()
 
 files = glob.glob(a.INPUT_FILE)
-
 idPattern = re.compile(".*([0-9]{7}).*")
+
+def createLookup(arr, key):
+    return dict([(str(item[key]), item) for item in arr])
+
+def readCsv(filename):
+    rows = []
+    fieldnames = []
+
+    with open(filename, 'r', encoding="utf8") as f:
+        lines = list(f)
+        reader = csv.DictReader(lines, skipinitialspace=True)
+        fieldnames = list(reader.fieldnames)
+        rows = list(reader)
+
+    print("Read %s rows from %s" % (len(rows), filename))
+
+    return (fieldnames, rows)
+
+def hasValue(lines, text):
+    found = False
+    for line in lines:
+        if text == line.strip():
+            found = True
+            break
+    return found
 
 def getValue(lines, text, multiline=False, delimeter=" ", endString=""):
     value = []
@@ -55,6 +80,10 @@ def writeCsv(filename, arr, headings):
             writer.writerow(row)
 
         print("Wrote %s rows to %s" % (len(arr), filename))
+
+existingRows = None
+if len(a.ANN_FILE) > 0:
+    existingFieldnames, existingRows = readCsv(a.ANN_FILE)
 
 rows = []
 for fn in files:
@@ -100,6 +129,9 @@ for fn in files:
     row["date"] = date
     row["dateType"] = type
 
+    # Check for digitized
+    row["digitized"] = 1 if hasValue(lines, "Playable Audio and Video") or hasValue(lines, "Play") else 0
+
     # Find contributors
     contributors = getValue(lines, "Name", multiline=True, delimeter=" | ")
     contributors = contributors.split(" | ")
@@ -112,4 +144,20 @@ for fn in files:
     row["summary"] = getValue(lines, "Summary", multiline=True, endString="Credits/Roles")
     rows.append(row)
 
-writeCsv(a.OUTPUT_FILE, rows, ["id", "date", "dateType", "title", "contributors", "summary"])
+fieldnames = ["id", "date", "digitized", "dateType", "title", "contributors", "summary"]
+
+if existingRows is not None:
+
+    if len(rows) != len(existingRows):
+        print("Warning: existing rowcount is %s, current rowcount is %s" % (len(existingRows), len(rows)))
+
+    rowLookup = createLookup(existingRows, "id")
+
+    for field in existingFieldnames:
+        if field not in fieldnames:
+            for i, row in enumerate(rows):
+                rows[i][field] = rowLookup[row["id"]][field] if row["id"] in rowLookup else ""
+
+    fieldnames = existingFieldnames
+
+writeCsv(a.OUTPUT_FILE, rows, fieldnames)
